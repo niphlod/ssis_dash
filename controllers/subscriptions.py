1b5f8e9a403f3fc4b6c8a01fa4d37f64b6b0102a
niphlod <niphlod@gmail.com>
2016-01-20 23:53:42 +0100

    v 0.0.33
    fixes #2, among fixing some minor issues with RSS generation

--- controllers/subscriptions.py
+++ controllers/subscriptions.py
@@ -20,6 +20,38 @@ import gluon.contrib.rss2 as rss2
 from gluon.storage import List
 import hashlib
 import os
+from collections import defaultdict
+import time
+import glob
+
+CACHE_FOR = 600
+PRUNING = CACHE_FOR * 100
+
+
+class NoOutput:
+    def __init__(self):
+        pass
+
+    def publish(self, handler):
+        pass
+
+
+class CDATARSS2(rss2.RSSItem):
+    def __init__(self, **kwargs):
+        rss2.RSSItem.__init__(self, **kwargs)
+
+    def publish(self, handler):
+        self.do_not_autooutput_description = self.description.decode('utf8')
+        self.description = NoOutput()  # This disables the Py2GenRSS "Automatic" output of the description, which would be escaped.
+        rss2.RSSItem.publish(self, handler)
+
+    def publish_extensions(self, handler):
+        handler._write('<%s><![CDATA[%s]]></%s>' % ("description", self.do_not_autooutput_description, "description"))
+
+
+def TDNW(el):
+    return TD(el, _nowrap='')
+
 
 def feed():
     request.args = List(request.raw_args.split('/'))
@@ -31,11 +63,22 @@ def feed():
     request.extension = 'rss'
     rargs = List(request.raw_args.split('/'))
     fname = "%s.rss" % hashlib.md5('/'.join(rargs)).hexdigest()
-    fpath = os.path.join(request.folder, 'private', 'temp_feeds', fname)
-    if os.path.isfile(fpath) and time.time() - os.path.getmtime(fpath) < (600):
-        with open(fpath, 'rb') as g:
-            rtn = g.read()
-        return rtn
+    basepath = os.path.join(request.folder, 'private', 'temp_feeds')
+    fpath = os.path.join(basepath, fname)
+    lock_file = fpath + '.__lock'
+    return_cached = os.path.isfile(lock_file)
+    if not return_cached:
+        return_cached = os.path.isfile(fpath) and time.time() - os.path.getmtime(fpath) < CACHE_FOR
+    if return_cached:
+        x = 0
+        while x < 60:
+            x += 1
+            if os.path.isfile(fpath):
+                with open(fpath, 'rb') as g:
+                    rtn = g.read()
+                return rtn
+    with open(lock_file, 'w') as g:
+        g.write('%s' % request.utcnow)
     folder = rargs(2) if rargs(1) == 'folder' else 'all'
     project = rargs(4) if rargs(3) == 'project' else 'all'
     status = rargs(6) if rargs(5) == 'status' else 'all'
@@ -50,7 +93,12 @@ def feed():
             status = k
             break
     res = read_and_exec('package-list.sql', placeholders=(HOURSPAN, folder_pattern, project_name, package_pattern, status), as_dict=True)
+    msgs = read_and_exec('package-messages.sql', placeholders=(HOURSPAN, folder_pattern, project_name, package_pattern, status), as_dict=True)
     res = massage_resultset(res)
+    msgs = massage_resultset(msgs)
+    msgs_dict = defaultdict(list)
+    for msg in msgs:
+        msgs_dict[msg.operation_id].append(msg)
     title = "SSIS Dashboard: Executions for %s" % request.args(0)
     title += folder != 'all' and ', folder %s' % folder or ''
     title += project != 'all' and ', project %s' % project or ''
@@ -59,26 +107,76 @@ def feed():
     items = []
     for entry in res:
         link = URL('console', 'overview', host=True, scheme='https', extension='', args=[request.args(0), 'folder', entry.folder_name, 'project',
-                            entry.project_name, 'status', 'all', 'package', entry.package_name, 'execution', entry.execution_id])
-        items.append(
-            rss2.RSSItem(
-               title=entry.package_name,
-               link=link,
-               guid=link + 'now',
-               description="Status : %s\n Warnings: %s\n Errors: %s\n Elapsed (min): %s" % (
-                    STATUS_CODES[entry.status], entry.warnings, entry.errors, entry.elapsed_time_min),
-               pubDate=entry.start_time.replace(' ', 'T'),
-               )
+                   entry.project_name, 'status', 'all', 'package', entry.package_name, 'execution', entry.execution_id])
+        detailed_status = [
+            ['Status', STATUS_CODES[entry.status]],
+            ['Elapsed (min)', entry.elapsed_time_min]
+        ]
+        errors = None
+        if entry.execution_id in msgs_dict:
+            detailed_status.extend([
+                ['Errors', entry.errors],
+                ['Warnings', entry.warnings],
+            ])
+            err_rows = msgs_dict[entry.execution_id]
+            messages = [[TDNW('Event Name'), TDNW('Message Time'), TDNW('Message'), TDNW('Package'),
+                        TDNW('Package Path'), TDNW('Subcomponent Name'), TDNW('Execution Path')]]
+            for msg in err_rows:
+                messages.append(
+                    [TDNW(msg.event_name), TDNW(msg.message_time), TD(msg.message), TDNW(msg.package_name),
+                     TD(msg.package_path), TDNW(msg.subcomponent_name), TD(msg.execution_path)]
+                )
+            errors = TABLE(
+                [TR([a for a in msg]) for msg in messages],
+                _border=1
             )
+        detailed_status = TABLE(
+            [TR([TD(el) for el in row]) for row in detailed_status]
+        )
+        if errors:
+            detailed_status = detailed_status.xml() + '<hr />' + errors.xml()
+        else:
+            detailed_status = detailed_status.xml()
+        items.append(
+            CDATARSS2(
+                title="%s - %s - (%s)" % (entry.execution_id, entry.package_name, STATUS_CODES[entry.status]),
+                link=link,
+                author="%s/%s@%s" % (entry.folder_name, entry.project_name, request.args(0)),
+                guid=link + 'now',
+                description=detailed_status,
+                pubDate=entry.start_time.replace(' ', 'T'),
+            )
+        )
     rss = rss2.RSS2(title=title,
                     link=URL(args=request.args, scheme='https', host=True),
                     description="Execution Packages",
-                    ttl=10*60,
-                    lastBuildDate=request.now,
+                    ttl=CACHE_FOR,
+                    lastBuildDate=request.utcnow,
                     items=items)
     rtn = rss.to_xml(encoding='utf-8')
     if not os.path.exists(os.path.dirname(fpath)):
         os.makedirs(os.path.dirname(fpath))
     with open(fpath, 'wb') as g:
         g.write(rtn)
+    try:
+        os.unlink(lock_file)
+    except:
+        pass
+
+    # can we do some cleaning ?
+    allfiles = glob.glob(os.path.join(basepath, '*.rss'))
+    for fpath in allfiles:
+        if os.path.isfile(fpath) and time.time() - os.path.getmtime(fpath) > PRUNING:
+            try:
+                os.unlink(fpath)
+            except:
+                pass
+    # this should never happen
+    allfiles = glob.glob(os.path.join(basepath, '*.__lock'))
+    for fpath in allfiles:
+        if os.path.isfile(fpath) and time.time() - os.path.getmtime(fpath) > 120:
+            try:
+                os.unlink(fpath)
+            except:
+                pass
     return rtn
---
+++ queries/package-messages.sql
@@ -0,0 +1,34 @@
+DECLARE @hourspan INT = ?;
+DECLARE @folderNamePattern NVARCHAR(100) = ?;
+DECLARE @projectNamePattern NVARCHAR(100) = ?;
+DECLARE @packageNamePattern NVARCHAR(100) = ?;
+DECLARE @statusFilter INT = ?;
+
+
+
+with CTEex as (
+SELECT TOP 100
+            e.execution_id
+    FROM    [catalog].executions e
+    WHERE   e.folder_name LIKE @folderNamePattern
+            AND e.project_name LIKE @projectNamePattern
+            AND e.package_name LIKE @packageNamePattern
+            AND e.start_time >= DATEADD(HOUR, -@hourspan, SYSDATETIME())
+            AND ( e.[status] = @statusFilter
+                  OR @statusFilter = 0
+                )
+    ORDER BY e.execution_id DESC
+)
+
+SELECT
+	   operation_id,
+	   FORMAT(message_time, 'yyyy-MM-dd HH:mm:ss') AS message_time ,
+	   [message] ,
+	   package_name ,
+	   package_path ,
+	   subcomponent_name ,
+	   execution_path ,
+	   event_name
+FROM    [catalog].event_messages e
+inner join CTEex on CTEex.execution_id = e.operation_id
+WHERE   (event_name =  'OnWarning' OR event_name = 'OnError')
